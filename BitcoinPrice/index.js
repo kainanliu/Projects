ee0c58eeeb445b8daf3126d95774ad83c9b2592a
kainanliu <kainanliu@hotmail.com>
2022-10-31 00:12:28 -0500

    Add API and APP functionality is completed

--- BitcoinPrice/index.js
+++ BitcoinPrice/index.js
@@ -13,15 +13,42 @@ app.get('/', (req, res) => {
 });
 
 app.get('/dollar', (req, res) =>{
-    res.render('dollar');
+    let API = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+    request(API, (error, response, body) =>{
+        if (!error && response.statusCode == 200) {
+            let data = JSON.parse(body);
+            let currency = data.bpi.USD.code;
+            let currencySymbol = data.bpi.USD.symbol;
+            let rate = data.bpi.USD.rate;
+            res.render('dollar', { code: currency, symbol: currencySymbol, rate: rate });
+        }
+    });
 });
 
 app.get('/pound', (req, res) =>{
-    res.render('pound');
+    let API = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+    request(API, (error, response, body) =>{
+        if (!error && response.statusCode == 200) {
+            let data = JSON.parse(body);
+            let currency = data.bpi.GBP.code;
+            let currencySymbol = data.bpi.GBP.symbol;
+            let rate = data.bpi.GBP.rate;
+            res.render('pound', { code: currency, symbol: currencySymbol, rate: rate });
+        }
+    });
 });
 
 app.get('/eur', (req, res) =>{
-    res.render('eur');
+    let API = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+    request(API, (error, response, body) =>{
+        if (!error && response.statusCode == 200) {
+            let data = JSON.parse(body);
+            let currency = data.bpi.EUR.code;
+            let currencySymbol = data.bpi.EUR.symbol;
+            let rate = data.bpi.EUR.rate;
+            res.render('eur', { code: currency, symbol: currencySymbol, rate: rate });
+        }
+    });
 });
 
 app.get('/index', (req, res) =>{
