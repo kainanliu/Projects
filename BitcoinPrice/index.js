66d1c4dea225c9a7cc2af68130f95a812736eeff
kainanliu <kainanliu@hotmail.com>
2022-11-02 00:40:08 -0500

    Add styles to the APP

--- BitcoinPrice/index.js
+++ BitcoinPrice/index.js
@@ -1,60 +1,78 @@
-const express = require('express');
+//This will enable all the modules that we need
+const express = require("express");
 const app = express();
-const request = require('request');
-
+const request = require("request");
+//We are setting the port to 3000
 const port = process.env.PORT || 3000;
-
-app.use(express.static('public'));
-
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-    res.render('index');
+//This will let us use the styling from the public folder
+app.use(express.static("public"));
+//This will set the view engine to ejs so that we do not need to type .ejs at the end of ejs files
+app.set("view engine", "ejs");
+//This is our home route that take us to the index page
+app.get("/", (req, res) => {
+  res.render("index");
 });
-
-app.get('/dollar', (req, res) =>{
-    let API = 'https://api.coindesk.com/v1/bpi/currentprice.json';
-    request(API, (error, response, body) =>{
-        if (!error && response.statusCode == 200) {
-            let data = JSON.parse(body);
-            let currency = data.bpi.USD.code;
-            let currencySymbol = data.bpi.USD.symbol;
-            let rate = data.bpi.USD.rate;
-            res.render('dollar', { code: currency, symbol: currencySymbol, rate: rate });
-        }
-    });
+//This will take us to the /dollar page
+app.get("/dollar", (req, res) => {
+  //store API into a var that we can use
+  let API = "https://api.coindesk.com/v1/bpi/currentprice.json";
+  request(API, (error, response, body) => {
+    //if nothing goes wrong, generate the price quote for USD
+    if (!error && response.statusCode == 200) {
+      //this will parse the body into JavaScript so that we can use the data from JSON
+      let data = JSON.parse(body);
+      //this will store code, description, and rate into varables
+      let currency = data.bpi.USD.code;
+      let currencyDescription = data.bpi.USD.description;
+      let rate = data.bpi.USD.rate;
+      //this will render the dollar.ejs file which contains the currency description, currency, and price
+      res.render("dollar", {
+        code: currency,
+        description: currencyDescription,
+        rate: rate,
+      });
+    }
+  });
 });
-
-app.get('/pound', (req, res) =>{
-    let API = 'https://api.coindesk.com/v1/bpi/currentprice.json';
-    request(API, (error, response, body) =>{
-        if (!error && response.statusCode == 200) {
-            let data = JSON.parse(body);
-            let currency = data.bpi.GBP.code;
-            let currencySymbol = data.bpi.GBP.symbol;
-            let rate = data.bpi.GBP.rate;
-            res.render('pound', { code: currency, symbol: currencySymbol, rate: rate });
-        }
-    });
+//Same concepts applied as in app.get for dollar
+app.get("/pound", (req, res) => {
+  let API = "https://api.coindesk.com/v1/bpi/currentprice.json";
+  request(API, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      let data = JSON.parse(body);
+      let currency = data.bpi.GBP.code;
+      let currencyDescription = data.bpi.GBP.description;
+      let rate = data.bpi.GBP.rate;
+      res.render("pound", {
+        code: currency,
+        description: currencyDescription,
+        rate: rate,
+      });
+    }
+  });
 });
-
-app.get('/eur', (req, res) =>{
-    let API = 'https://api.coindesk.com/v1/bpi/currentprice.json';
-    request(API, (error, response, body) =>{
-        if (!error && response.statusCode == 200) {
-            let data = JSON.parse(body);
-            let currency = data.bpi.EUR.code;
-            let currencySymbol = data.bpi.EUR.symbol;
-            let rate = data.bpi.EUR.rate;
-            res.render('eur', { code: currency, symbol: currencySymbol, rate: rate });
-        }
-    });
+//Same concepts applied as in app.get for dollar
+app.get("/eur", (req, res) => {
+  let API = "https://api.coindesk.com/v1/bpi/currentprice.json";
+  request(API, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      let data = JSON.parse(body);
+      let currency = data.bpi.EUR.code;
+      let currencyDescription = data.bpi.EUR.description;
+      let rate = data.bpi.EUR.rate;
+      res.render("eur", {
+        code: currency,
+        description: currencyDescription,
+        rate: rate,
+      });
+    }
+  });
 });
-
-app.get('/index', (req, res) =>{
-    res.render('index');
+//This will get us back to the home page
+app.get("/index", (req, res) => {
+  res.render("index");
 });
-
-app.listen(port, () =>{
-    console.log(`Bitcoin Price APP listen on port ${port}`);
+//this will tell us which port the app is running on
+app.listen(port, () => {
+  console.log(`Bitcoin Price APP listen on port ${port}`);
 });
